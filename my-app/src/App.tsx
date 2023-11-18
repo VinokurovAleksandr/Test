30d301d7c0da1cfe7e833613fcdd55e283d9fc0c
Aleksandr Vinokurov <98280490+VinokurovAleksandr@users.noreply.github.com>
2023-11-18 12:31:44 +0200

    test TS

---
+++ my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+ 
+  const temps: readonly number[] = [
+    30, 31, 32, 24,
+  ];
+
+
+  console.log(temps)
+
+
+  return (
+    <div>
+      
+ </div>
+   
+  );
+}
+
+export default App;
---
+++ my-app/src/TypeScript/primitives.ts
@@ -0,0 +1,11 @@
+
+function Primitives() {
+    let age: number = 5;
+age = 5;
+
+console.log(age);
+
+}
+
+
+export default Primitives ;
